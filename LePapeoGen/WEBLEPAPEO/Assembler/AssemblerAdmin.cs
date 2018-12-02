f03fddba9a082ff0d9afdf36d0a117c59b7ea467
iCbsp <carlos_bsp@hotmail.com>
2018-12-02 14:32:15 +0100

    Controller de Admin creado. También he modificado AdminEN.cs

---
+++ LePapeoGen/WEBLEPAPEO/Assembler/AssemblerAdmin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LePapeoGenNHibernate.EN.LePapeo;
+
+namespace LePapeo.Models
+{
+    public class AssemblerAdmin
+    {
+        public AdminViewModel ConvertENToModelUI(AdminEN adminEN)
+        {
+            AdminViewModel admin = new AdminViewModel();
+            admin.id = adminEN.Id;
+            admin.email = adminEN.Email;
+            admin.p_pass = adminEN.Pass; //LePapeoGenNHibernate.Utils.Util.GetEncondeMD5(adminEN.Pass)
+            admin.p_fecha_inscripcion = adminEN.Fecha_inscripcion;
+
+            return admin;
+
+
+        }
+        public IList<AdminViewModel> ConvertListENToModel (IList<AdminEN> ens){
+            IList<AdminViewModel> arts = new List<AdminViewModel>();
+            foreach (AdminEN en in ens)
+            {
+                arts.Add(ConvertENToModelUI(en));
+            }
+            return arts;
+        }
+        
+    }
+}
---
+++ LePapeoGen/WEBLEPAPEO/Controllers/AdminController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LePapeo.Models;
+using LePapeoGenNHibernate.CAD.LePapeo;
+using LePapeoGenNHibernate.CEN.LePapeo;
+using LePapeoGenNHibernate.EN.LePapeo;
+
+
+namespace WEBLEPAPEO.Controllers
+{
+    public class AdminController : BasicController
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            AdminCEN adminCEN = new AdminCEN();
+            IEnumerable<AdminEN> listaAdminEN = adminCEN.ReadAll(0, -1).ToList(); //0 posicion inicial y -1 todos los elementos/infinito
+            return View(listaAdminEN);
+            //return View();
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int id)
+        {
+            AdminViewModel admin = null;
+            SessionInitialize();
+            AdminEN adminEN = new AdminCAD(session).ReadOIDDefault(id);
+            admin = new AssemblerAdmin().ConvertENToModelUI(adminEN);
+            SessionClose();
+            return View(adminEN); //supuestamente deberia de ser return View(admin);
+            //return View();
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            AdminEN adminEN = new AdminEN();
+
+            return View(adminEN);
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(AdminEN adminEN)//FormCollection collection
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                SessionInitialize();
+                AdminCAD adminCAD = new AdminCAD();
+                adminCAD.New_(adminEN);
+                SessionClose();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            AdminViewModel admin = null;
+            SessionInitialize();
+            AdminEN adminEN = new AdminCAD(session).ReadOIDDefault(id);
+            admin = new AssemblerAdmin().ConvertENToModelUI(adminEN);
+
+            SessionClose();
+            return View(adminEN); //supuestamente deberia de ser return View(admin);
+            //return View();
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(AdminEN adminEN)//int id, FormCollection collection || AdminViewModel admin
+        {
+            try
+            {
+
+                AdminViewModel admin = new AssemblerAdmin().ConvertENToModelUI(adminEN);
+                AdminCEN adminCEN = new AdminCEN();
+                adminCEN.Modify(admin.id, admin.email, admin.p_pass, admin.p_fecha_inscripcion); //Utils.Util.GetEncondeMD5 (p_pass); || LePapeoGenNHibernate.Utils.Util.GetEncondeMD5(admin.p_pass)
+                //return View();
+                return RedirectToAction("Index");
+
+
+                /*
+                SessionInitialize();
+                AdminCAD adminCAD = new AdminCAD(session);
+                AdminCEN adminCEN = new AdminCEN();
+                adminCEN.Modify(admin.id, admin.email, admin.p_pass, admin.p_fecha_inscripcion);
+                SessionClose();
+                return View();
+                */
+
+                //return RedirectToAction("PorCategoria", new { id = art.IdCategoria });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            try
+            {
+                // TODO: Add delete logic here
+                //int idCategoria = -1;
+                SessionInitialize();
+                AdminCAD adminCAD = new AdminCAD(session);
+                AdminCEN adminCEN = new AdminCEN(adminCAD);
+                AdminEN adminEN = adminCEN.ReadOID(id);
+                AdminViewModel art = new AssemblerAdmin().ConvertENToModelUI(adminEN);
+                //idCategoria = art.IdCategoria;
+                SessionClose();
+
+                new AdminCEN().Destroy(id);
+
+
+                //return RedirectToAction("PorCategoria", new { id = idCategoria });
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+            //return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
