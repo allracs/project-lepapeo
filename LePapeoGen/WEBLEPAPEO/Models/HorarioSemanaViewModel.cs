3748f1b1fa2549432e1df002be1292523ec957d7
SrJaguarr <dnavasdiaz97@gmail.com>
2018-12-13 00:42:28 +0100

    Retiradas 0,1 0,* de HorarioSemana

--- LePapeoGen/WEBLEPAPEO/Models/HorarioSemanaViewModel.cs
+++ LePapeoGen/WEBLEPAPEO/Models/HorarioSemanaViewModel.cs
@@ -10,7 +10,7 @@ namespace LePapeo.Models
     {
         [ScaffoldColumn(false)]
         public int id { get; set; }
-
+        /**
         [Display(Prompt = "Horario", Description = "Horario", Name = "Horario dia")]
         [Required(ErrorMessage = "Debe indicar el horario del dia.")]
         public System.Collections.Generic.IList<LePapeoGenNHibernate.EN.LePapeo.HorarioDiaEN> Horario_dia { get; set; }
@@ -18,7 +18,7 @@ namespace LePapeo.Models
         [Display(Prompt = "Restaurante", Description = "Restaurante", Name = "Restaurante")]
         [Required(ErrorMessage = "Debe indicar el restaurante.")]
         public System.Collections.Generic.IList<LePapeoGenNHibernate.EN.LePapeo.RestauranteEN> Restaurante { get; set; }
-
+        **/
 
     }
 }
