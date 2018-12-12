fb6d7a258126696af6719a4d15daf4083acb44e5
SrJaguarr <dnavasdiaz97@gmail.com>
2018-12-12 14:38:35 +0100

    HorarioDia y HorarioSemana (Falta meter ReadOID y ReadAll en el modelo)

---
+++ LePapeoGen/WEBLEPAPEO/Controllers/HorarioSemanaController.cs
@@ -0,0 +1,127 @@
+﻿using LePapeo.Models;
+using LePapeoGenNHibernate.CAD.LePapeo;
+using LePapeoGenNHibernate.CEN.LePapeo;
+using LePapeoGenNHibernate.EN.LePapeo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBLEPAPEO.Controllers
+{
+    public class HorarioSemanaController : BasicController
+    {
+        // GET: HorarioSemana
+        public ActionResult Index()
+        {
+            HorarioSemanaCEN semCEN = new HorarioSemanaCEN();
+            IList<HorarioSemanaEN> listsemEN = semCEN.ReadAll(0, -1);
+            IEnumerable<HorarioSemanaViewModel> listsem = new AssemblerHorarioSemana().ConvertListENToModel(listsemEN);
+
+            return View(listsem);
+        }
+
+        // GET: HorarioSemana/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HorarioSemana/Create
+        public ActionResult Create()
+        {
+            HorarioSemanaViewModel sem = new HorarioSemanaViewModel();
+            return View(sem);
+        }
+
+        // POST: HorarioSemana/Create
+        [HttpPost]
+        public ActionResult Create(HorarioSemanaViewModel sem)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                HorarioSemanaCEN cen = new HorarioSemanaCEN();
+                cen.New_();
+                
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Registrado/Edit/5
+        public ActionResult Edit(int id)
+        {
+            HorarioSemanaViewModel sem = null;
+            SessionInitialize();
+            HorarioSemanaEN semEN = new HorarioSemanaCAD(session).ReadOIDDefault(id);
+            sem = new AssemblerHorarioSemana().ConvertENToModelUI(semEN);
+            SessionClose();
+
+            return View(sem);
+        }
+
+        // POST: HorarioSemana/Edit/5
+        [HttpPost]
+        public ActionResult Edit(HorarioSemanaViewModel sem)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                HorarioSemanaCEN cen = new HorarioSemanaCEN();
+                cen.Modify(sem.id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HorarioSemana/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                SessionInitialize();
+                HorarioSemanaCAD semCAD = new HorarioSemanaCAD(session);
+                HorarioSemanaCEN cen = new HorarioSemanaCEN(semCAD);
+                HorarioSemanaEN semEN = cen.ReadOID(id);
+                HorarioSemanaViewModel sem = new AssemblerHorarioSemana().ConvertENToModelUI(semEN);
+
+                SessionClose();
+
+               
+                return View(sem);
+            }
+            catch
+            {
+                //Meter aqui el mensaje de error
+                return View();
+            }
+        }
+
+        // POST: HorarioSemana/Delete/5
+        [HttpPost]
+        public ActionResult Delete(HorarioSemanaViewModel sem)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                new HorarioSemanaCEN().Destroy(sem.id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ LePapeoGen/WEBLEPAPEO/Models/HorarioSemanaViewModel.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LePapeo.Models
+{
+    public class HorarioSemanaViewModel
+    {
+        [ScaffoldColumn(false)]
+        public int id { get; set; }
+
+        [Display(Prompt = "Horario", Description = "Horario", Name = "Horario dia")]
+        [Required(ErrorMessage = "Debe indicar el horario del dia.")]
+        public System.Collections.Generic.IList<LePapeoGenNHibernate.EN.LePapeo.HorarioDiaEN> Horario_dia { get; set; }
+
+        [Display(Prompt = "Restaurante", Description = "Restaurante", Name = "Restaurante")]
+        [Required(ErrorMessage = "Debe indicar el restaurante.")]
+        public System.Collections.Generic.IList<LePapeoGenNHibernate.EN.LePapeo.RestauranteEN> Restaurante { get; set; }
+
+
+    }
+}
